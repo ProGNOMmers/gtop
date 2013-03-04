f577b01cd785edc18691155a5678f638b474c662
Maurizio De Santis <desantis.maurizio@gmail.com>
2013-03-04 02:18:12 +0100

    fix indentation

--- lib/gtop/load_average.rb
+++ lib/gtop/load_average.rb
@@ -2,10 +2,10 @@ require 'gtop'
 
 module GTop
   class LoadAverage < FFI::Struct
-layout :flags,       :guint64,
-       :loadavg,    [:double, 3],
-       :nr_running,  :guint64,
-       :nr_tasks,    :guint64,
-       :last_pid,    :guint64
+    layout :flags,       :guint64,
+           :loadavg,    [:double, 3],
+           :nr_running,  :guint64,
+           :nr_tasks,    :guint64,
+           :last_pid,    :guint64
   end
 end
