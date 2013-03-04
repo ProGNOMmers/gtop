6ee6ef054e457584e05bbf06b5b5ef2d0ed7d10c
Maurizio De Santis <desantis.maurizio@gmail.com>
2013-03-04 01:56:12 +0100

    GLIBTOP_NCPU

--- lib/gtop/cpu.rb
+++ lib/gtop/cpu.rb
@@ -2,6 +2,8 @@ require 'gtop'
 
 module GTop
   class Cpu < FFI::Struct
+    NCPU = 32
+
     layout :flags,         :guint64,
            :total,         :guint64,
            :user,          :guint64,
@@ -12,14 +14,14 @@ module GTop
            :irq,           :guint64,
            :softirq,       :guint64,
            :frequency,     :guint64,
-           :xcpu_total,   [:guint64, 32],
-           :xcpu_user,    [:guint64, 32],
-           :xcpu_nice,    [:guint64, 32],
-           :xcpu_sys,     [:guint64, 32],
-           :xcpu_idle,    [:guint64, 32],
-           :xcpu_iowait,  [:guint64, 32],
-           :xcpu_irq,     [:guint64, 32],
-           :xcpu_softirq, [:guint64, 32],
+           :xcpu_total,   [:guint64, NCPU],
+           :xcpu_user,    [:guint64, NCPU],
+           :xcpu_nice,    [:guint64, NCPU],
+           :xcpu_sys,     [:guint64, NCPU],
+           :xcpu_idle,    [:guint64, NCPU],
+           :xcpu_iowait,  [:guint64, NCPU],
+           :xcpu_irq,     [:guint64, NCPU],
+           :xcpu_softirq, [:guint64, NCPU],
            :xcpu_flags,    :guint64
   end
 end
